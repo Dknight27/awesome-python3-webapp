72e3a3549833c3f6beb5a83530ff80d74da10323
Dknight27 <978864703@qq.com>
2019-09-24 10:10:30 +0800

    day-03 基本实现ormorm

--- www/app.py
+++ www/app.py
@@ -1,3 +1,7 @@
+#经测试，现在这个程序返回的网页不能被正确解析，原因不明
+#表现为：Edge网页显示body中字符串内容（包括标签）
+#Google网页返回一个下载
+#解决：教程过时，参考aiohttp最新文档
 import logging;logging.basicConfig(level=logging.INFO)#打印日志信息
 
 import asyncio,os,json,time
@@ -6,16 +10,19 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>This is a body.<h1>')
+    return web.Response(text='Awesome!')
 
-@asyncio.coroutine
-def init(loop):
-    app=web.Application(loop=loop)
-    app.router.add_route('GET','/',index)
-    srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9999)#sh*t,忘了加括号
-    logging.info('server started at http://118.24.81.246:9999')
-    return srv
+# 这样封装无法正常运行
+# @asyncio.coroutine
+# def init():
+#     app=web.Application()
+#     app.add_routes([web.get('/',index)])
+#     # srv=yield from loop.create_server(app.make_handler(),'172.27.0.3',9999)#sh*t,忘了加括号
+#     web.run_app(app)
+#     logging.info('server started')
+#     return app
 
-loop=asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+app = web.Application()
+app.add_routes([web.get('/',index)])#对当前ip:8080进行监听
+logging.info('server started')
+web.run_app(app)
